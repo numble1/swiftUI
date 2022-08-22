3e59f196cdff6dc9b52f61a64e955353d322023c
WangJie <641156127@qq.com>
2022-08-22 08:51:52 +0800

    第一次提交

---
+++ AppForiOS15/AppForiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  AppForiOS15App.swift
+//  AppForiOS15
+//
+//  Created by shyt_cxp on 2022/8/18.
+//
+
+import SwiftUI
+
+@main
+struct AppForiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
